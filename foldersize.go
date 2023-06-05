4522d92ba64eaf50f71fbb2be2de35c54fe71274
xmechaniqx <93098336+xmechaniqx@users.noreply.github.com>
2023-06-05 15:59:28 +0400

    Update foldersize.go

--- foldersize.go
+++ foldersize.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	var Arr []string
 	target := make(map[string]float64)
 	dir := "N:/"
@@ -28,7 +30,7 @@ func main() {
 	for _, v := range Arr {
 		// DirSize(v)
 		fmt.Println(v)
-		if DirSizeMB(v) >= 250 {
+		if DirSizeMB(v) >= 305 {
 			target[v] = DirSizeMB(v)
 		}
 		// fmt.Printf("%10.2f\n %10s", DirSizeMB(v), names[k])
@@ -41,22 +43,68 @@ func main() {
 	write(target)
 	// fmt.Println(target)
 
+	// Код для измерения
+	duration := time.Since(start)
+
+	// Отформатированная строка,
+	// например, "2h3m0.5s" или "4.503μs"
+	fmt.Println(duration)
+
 }
+
+// func DirSizeMB(path string) float64 {
+// 	var dirSize int64 = 0
+
+// 	readSize := func(path string, file os.FileInfo, err error) error {
+// 		// if file.IsDir() {
+// 		// 	dirSize += file.Size()
+// 		// 	fmt.Println(path, file.Size())
+// 		// 	return nil // Проигнорируем директории
+// 		// }
+// 		if !file.IsDir() {
+// 			dirSize += file.Size()
+// 			// fmt.Println(path, file.Size())
+// 			fmt.Println(path)
+// 		}
+
+// 		return nil
+// 	}
+
+// 	filepath.Walk(path, readSize)
+
+// 	sizeMB := float64(dirSize) / 1024.0 / 1024.0
+
+// 	return sizeMB
+// }
+
 func DirSizeMB(path string) float64 {
-	var dirSize int64 = 0
-
+	sizes := make(chan int64)
 	readSize := func(path string, file os.FileInfo, err error) error {
-		if !file.IsDir() {
-			dirSize += file.Size()
-			fmt.Println(path, file.Size())
+		if err != nil || file == nil {
+			return nil // Ignore errors
 		}
+		if !file.IsDir() {
+			fmt.Println(path, file.Size())
+			sizes <- file.Size()
 
+		}
 		return nil
 	}
 
-	filepath.Walk(path, readSize)
+	go func() {
+		filepath.Walk(path, readSize)
+		close(sizes)
+	}()
 
-	sizeMB := float64(dirSize) / 1024.0 / 1024.0
+	size := int64(0)
+	for s := range sizes {
+
+		size += s
+	}
+
+	sizeMB := float64(size) / 1024.0 / 1024.0
+
+	// sizeMB = Round(sizeMB, 0.5, 2)
 
 	return sizeMB
 }
